f70388b4fa374e926f8aaa3fc8179fa6c4218901
savanna chiang <savannaac@gmail.com>
2021-07-21 20:33:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, callback, index) {
+    let accumulator = index ? index : array[0]
+    let i = index ? 0:1
+
+    for (; i < array.length; i++) {
+       accumulator = callback(array[i], accumulator)
+    }
+    return accumulator
+}
